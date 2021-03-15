a8130bc46ed7724949c7133b1db0125bea6442ea
Dan Ribbens <dan.ribbens@gmail.com>
2021-03-15 17:36:07 -0400

    feat: postman collection

--- collections/Users.js
+++ collections/Users.js
@@ -4,12 +4,13 @@
  * @returns {boolean}
  */
 function isGuest ({ req: { user } }) {
-  return user.collection === 'admins' || !user;
+  return !user || user.collection === 'admins';
 }
 
 /**
  * middleware that returns true for users making requests on their own collection item or admins
  * @param user
+ * @param id
  * @returns {boolean}
  */
 function isSelf ({ req: {user, id} }) {
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Sample repository for Payload CMS authentication
+
+Originally written for the post: 
+https://payloadcms.com/blog/authentication-basics
